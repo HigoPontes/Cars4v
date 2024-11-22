0bd11a848096b4c467345a262caea05493045505
HigoPontes <Ponteshigo707@gmail.com>
2024-11-22 16:04:33 -0300

    Adicionei o usuario a tabela

--- aplicativo/admin.py
+++ aplicativo/admin.py
@@ -4,7 +4,7 @@ from aplicativo.models import Cars
 # Register your models here.
 
 class CarsFilter(admin.ModelAdmin):
-    list_display = ("id","model","brand","year")
+    list_display = ("id","user","model","brand","year")
     list_display_links = ("id","model","brand","year")
     list_filter = ("model","year")
     search_fields = ("brand","model","year")
